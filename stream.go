5b34db957f79c1c1718ebddb82a24860a88e26f1
Jeremy Liu <ksloveyuan@gmail.com>
2019-09-11 00:03:40 +0800

    Add more configuration and Race method

--- stream.go
+++ stream.go
@@ -2,6 +2,8 @@ package channelutil
 
 import (
 	"sync"
+	"runtime"
+	"time"
 )
 
 type ChannelStream struct {
@@ -10,6 +12,7 @@ type ChannelStream struct {
 	ape actionPerError
 	optionFuncs []OptionFunc
 	hasError bool
+	errors []error
 }
 
 type Result struct {
@@ -27,28 +30,35 @@ const (
 type SeedFunc func(seedChan chan <- Result)
 type PipeFunc func(result Result) Result
 type HarvestFunc func(result Result)
+type RaceFunc func(result Result) bool
 
-type OptionFunc func(cs *ChannelStream) func()
+type OptionFunc func(cs *ChannelStream)
 
 func NewChannelStream(seedFunc SeedFunc, optionFuncs ...OptionFunc) *ChannelStream {
 	cs := &ChannelStream{
-		dataChannel: make(chan Result, 4),
-		workers:     4,
+		workers:     runtime.NumCPU() ,
 		optionFuncs: optionFuncs,
 	}
 
 	for _, of := range optionFuncs {
-		of(cs)()
+		of(cs)
 	}
 
+	cs.dataChannel  = make(chan Result, cs.workers)
+
 	go func() {
-		inputChan := make(chan Result, 4)
+		inputChan := make(chan Result, cs.workers)
 
 		go seedFunc(inputChan)
 
 		for res := range inputChan {
+			if res.Err != nil {
+				cs.errors = append(cs.errors, res.Err)
+			}
+
 			if !cs.hasError && res.Err != nil{
 				cs.hasError = true
+				cs.dataChannel <- res
 			}
 
 			if cs.hasError && cs.ape == stop {
@@ -57,25 +67,33 @@ func NewChannelStream(seedFunc SeedFunc, optionFuncs ...OptionFunc) *ChannelStre
 
 			cs.dataChannel <- res
 		}
-		close(cs.dataChannel)
+
+		safeCloseChannel(cs.dataChannel)
+
 	}()
 
 	return cs
 }
 
-func StopWhenHasError(p *ChannelStream) func() {
-	return func() {
+func StopWhenHasError() func(p *ChannelStream) {
+	return func(p *ChannelStream) {
 		p.ape = stop
 	}
 }
 
-func ResumeWhenHasError(p *ChannelStream) func() {
-	return func() {
+func ResumeWhenHasError() func(p *ChannelStream) {
+	return func(p *ChannelStream) {
 		p.ape = resume
 	}
 }
 
-func (p *ChannelStream) Pipe(dataPipeFunc PipeFunc) *ChannelStream {
+func SetWorkers(workers int) func(p *ChannelStream) {
+	return func(p *ChannelStream) {
+		p.workers = workers
+	}
+}
+
+func (p *ChannelStream) Pipe(dataPipeFunc PipeFunc, optionFuncs ...OptionFunc) *ChannelStream {
 	seedFunc := func(dataPipeChannel chan <- Result) {
 		wg := &sync.WaitGroup{}
 		wg.Add(p.workers)
@@ -90,15 +108,55 @@ func (p *ChannelStream) Pipe(dataPipeFunc PipeFunc) *ChannelStream {
 
 		go func() {
 			wg.Wait()
-			close(dataPipeChannel)
+			safeCloseChannel(dataPipeChannel)
 		}()
 	}
 
-	return NewChannelStream(seedFunc, p.optionFuncs...)
+	mergeOptionFuncs := make([]OptionFunc, len(p.optionFuncs)+len(optionFuncs))
+	copy(mergeOptionFuncs[0:len(p.optionFuncs)], p.optionFuncs)
+	copy(mergeOptionFuncs[len(p.optionFuncs):], optionFuncs)
+
+	return NewChannelStream(seedFunc, mergeOptionFuncs...)
 }
 
-func (p *ChannelStream) Done(harvestFunc HarvestFunc) {
+func safeCloseChannel(dataPipeChannel chan<- Result) {
+	if len(dataPipeChannel) == 0 {
+		close(dataPipeChannel)
+	} else {
+		ticker := time.Tick(1 * time.Millisecond)
+		for range ticker {
+			if len(dataPipeChannel) == 0 {
+				close(dataPipeChannel)
+				break
+			}
+		}
+	}
+}
+
+func (p *ChannelStream) Race(raceFunc RaceFunc) {
+	for result := range p.dataChannel {
+		if raceFunc(result) {
+			break
+		}
+	}
+
+	go func() {
+		for range p.dataChannel {
+		}
+	}()
+}
+
+func (p *ChannelStream) Wait(harvestFunc HarvestFunc) (bool, []error) {
 	for result := range p.dataChannel {
 		harvestFunc(result)
 	}
+
+	return !p.hasError, p.errors
+}
+
+func (p *ChannelStream) Drain() (bool, []error) {
+	for range p.dataChannel {
+	}
+
+	return !p.hasError, p.errors
 }
--- stream_test.go
+++ stream_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/pkg/errors"
 )
 
-func TestChannelStream_Pipe_StopWhenHasError(t *testing.T) {
+func TestChannelStream_SecondPipe_StopWhenHasError(t *testing.T) {
 	stream := NewChannelStream(func(seedChan chan<- Result) {
 		for i := 1; i <= 10; i++ {
 			if i %2 == 1 {
@@ -16,10 +16,71 @@ func TestChannelStream_Pipe_StopWhenHasError(t *testing.T) {
 			}
 		}
 		close(seedChan)
-	}, StopWhenHasError)
+	})
+
+	stream2 := stream.Pipe(func(result Result) Result {
+		if result.Err == nil{
+			i:= result.Data.(int) * 2
+			fmt.Println("s2", i)
+			return Result{Data: i}
+		}
+
+		return Result{Err:result.Err}
+	}, StopWhenHasError(), SetWorkers(2))
 
 	harvestResult := 0
-	stream.Done(func(result Result) {
+	stream2.Wait(func(result Result) {
+		if result.Err == nil {
+			i := result.Data.(int)
+			fmt.Println("har", i)
+			harvestResult += i
+		} else {
+			fmt.Println("met error")
+		}
+	})
+
+	fmt.Println(harvestResult)
+}
+
+func TestChannelStream_Race(t *testing.T) {
+	stream := NewChannelStream(func(seedChan chan<- Result) {
+		for i := 1; i <= 10; i++ {
+			if i %2 == 1 {
+				seedChan <- Result{Data: i, Err: nil}
+			} else {
+				seedChan <- Result{Err:errors.New(fmt.Sprintf("error: %d", i))}
+			}
+		}
+		close(seedChan)
+	})
+
+	var error error
+	stream.Race(func(result Result) bool{
+		if result.Err == nil {
+			fmt.Println("skip", result.Data.(int))
+			return false
+		}
+		error = result.Err
+		return true
+	})
+
+	fmt.Println(error)
+}
+
+func TestChannelStream_Wait_StopWhenHasError(t *testing.T) {
+	stream := NewChannelStream(func(seedChan chan<- Result) {
+		for i := 1; i <= 10; i++ {
+			if i %2 == 1 {
+				seedChan <- Result{Data: i, Err: nil}
+			} else {
+				seedChan <- Result{Err:errors.New("An error")}
+			}
+		}
+		close(seedChan)
+	}, StopWhenHasError())
+
+	harvestResult := 0
+	stream.Wait(func(result Result) {
 		if result.Err == nil {
 			i := result.Data.(int)
 			harvestResult += i
@@ -29,7 +90,7 @@ func TestChannelStream_Pipe_StopWhenHasError(t *testing.T) {
 	fmt.Println(harvestResult)
 }
 
-func TestChannelStream_Pipe_ResumeWhenHasError(t *testing.T) {
+func TestChannelStream_Wait_ResumeWhenHasError(t *testing.T) {
 	stream := NewChannelStream(func(seedChan chan<- Result) {
 		for i := 1; i <= 10; i++ {
 			if i %2 == 1 {
@@ -39,13 +100,15 @@ func TestChannelStream_Pipe_ResumeWhenHasError(t *testing.T) {
 			}
 		}
 		close(seedChan)
-	}, ResumeWhenHasError)
+	}, ResumeWhenHasError())
 
 	harvestResult := 0
-	stream.Done(func(result Result) {
+	stream.Wait(func(result Result) {
 		if result.Err == nil {
 			i := result.Data.(int)
 			harvestResult += i
+		} else {
+			harvestResult -= 1
 		}
 	})
 
@@ -69,7 +132,7 @@ func TestChannelStream_Pipe_Twice(t *testing.T) {
 	})
 
 	harvestResult := 0
-	stream3.Done(func(result Result) {
+	stream3.Wait(func(result Result) {
 		i := result.Data.(int)
 		harvestResult += i
 	})
@@ -90,7 +153,7 @@ func TestChannelStream_Pipe(t *testing.T) {
 	})
 
 	harvestResult := 0
-	stream2.Done(func(result Result) {
+	stream2.Wait(func(result Result) {
 		i := result.Data.(int)
 		harvestResult += i
 	})
@@ -98,7 +161,7 @@ func TestChannelStream_Pipe(t *testing.T) {
 	fmt.Println(harvestResult)
 }
 
-func TestChannelStream_Done(t *testing.T) {
+func TestChannelStream_Wait(t *testing.T) {
 	stream := NewChannelStream(func(seedChan chan<- Result) {
 		for i := 1; i <= 100; i++ {
 			seedChan <- Result{Data: i, Err: nil}
@@ -107,7 +170,7 @@ func TestChannelStream_Done(t *testing.T) {
 	})
 
 	harvestResult := 0
-	stream.Done(func(result Result) {
+	stream.Wait(func(result Result) {
 		i := result.Data.(int)
 		harvestResult += i
 	})
